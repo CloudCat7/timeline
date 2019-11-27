e6ca5cd3bdc9e6eadcc3ef61a3643fb03cd1055d
poetsmurf <1481001728@qq.com>
2019-11-27 23:44:51 +0800

    update

--- src/test/java/com/testing/timeline/repository/MessageRepositoryTest.java
+++ src/test/java/com/testing/timeline/repository/MessageRepositoryTest.java
@@ -3,21 +3,28 @@ package com.testing.timeline.repository;
 import com.testing.timeline.domain.Message;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import java.util.Date;
 
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @DirtiesContext
+@AutoConfigureTestDatabase(replace = NONE)
 class MessageRepositoryTest {
 
     @Autowired
@@ -28,7 +35,7 @@ class MessageRepositoryTest {
     private Message message_1;
     private Date d1 = new Date();
 
-    @Before
+    @BeforeEach
     public void init() {
         message_1 = new Message.MessageBuilder().username("user_1")
                 .content("test_text_1")
@@ -37,14 +44,14 @@ class MessageRepositoryTest {
                 .build();
     }
 
-    @After
-    public void cleanup() { this.entityManager.clear(); }
+    @AfterEach
+    public void cleanup() { entityManager.clear(); }
 
     @Test
     public void testFindAllMessage() {
 
-        this.entityManager.persist(message_1);
-        Iterable<Message> messages = repo.findAll();
+        entityManager.persist(message_1);
+        Iterable<Message>messages = repo.findAll();
 
         int count = 0;
         for(Message message : messages) {
